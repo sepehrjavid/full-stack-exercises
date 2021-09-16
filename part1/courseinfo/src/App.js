a37b15401e1d7af4e9c569c4554e477d5b3a3135
sepehr javid <javid.sepehr77@gmail.com>
2021-09-16 14:10:40 +0300

    finish exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,28 +4,30 @@ import Content from "./components/Content";
 import Footer from "./components/Footer";
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ];
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
 
     return (
         <div>
-            <Header name={course}/>
-            <Content content={parts}/>
-            <Footer count={parts.reduce((prev, cur) => {
+            <Header name={course.name}/>
+            <Content content={course.parts}/>
+            <Footer count={course.parts.reduce((prev, cur) => {
                 return prev + cur.exercises
             }, 0)}/>
         </div>
