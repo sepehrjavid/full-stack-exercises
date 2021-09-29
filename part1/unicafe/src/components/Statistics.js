a42e4191f3b964c8adbf00aece66ca286347e70b
sepehr javid <javid.sepehr77@gmail.com>
2021-09-29 12:50:13 +0300

    Excercise 1.7

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const Statistics = ({goodCount, badCount, neutralCount}) => {
+    const totalVotes = badCount + goodCount + neutralCount;
     return (
         <div>
             <h2>Statistics</h2>
             <p>Good: {goodCount}</p>
             <p>Neutral: {neutralCount}</p>
             <p>Bad: {badCount}</p>
+            <p>All: {totalVotes}</p>
+            <p>Average: {(badCount * -1 + goodCount * 1) / totalVotes}</p>
+            <p>Positive: {goodCount / totalVotes}</p>
         </div>
     )
 };
