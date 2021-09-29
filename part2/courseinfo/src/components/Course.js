778fc13ca4090949ad4884d514b367cd88084ad8
sepehr javid <javid.sepehr77@gmail.com>
2021-09-29 15:48:47 +0300

    Exercise 2.3

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Course = ({course}) => {
+    const total = course.parts.map((part) => part.exercises).reduce((number, a) => a + number, 0);
+
     return (
         <div>
             <h1>{course.name}</h1>
             {course.parts.map((part) => <p key={part.id}>{part.name} {part.exercises}</p>)}
-            <b>total of {course.parts.map((part) => part.exercises).reduce((number, a) => a + number, 0)} exercises</b>
+            <b>total of {total} exercises</b>
         </div>
     )
 };
