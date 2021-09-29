4f62fb840d6359a18e3c7a01db2a91e82ce6e55a
sepehr javid <javid.sepehr77@gmail.com>
2021-09-29 12:55:02 +0300

    Excercise 1.8 and 1.9

--- part1/unicafe/src/components/Feedback.js
+++ part1/unicafe/src/components/Feedback.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Feedback = ({goodHandler, badHandler, neutralHandler}) => {
     return (
-        <>
+        <div>
             <h2>Give Feedback</h2>
             <button onClick={goodHandler}>good</button>
             <button onClick={neutralHandler}>neutral</button>
             <button onClick={badHandler}>bad</button>
-        </>
+        </div>
     )
 };
 
--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -2,17 +2,26 @@ import React from 'react'
 
 const Statistics = ({goodCount, badCount, neutralCount}) => {
     const totalVotes = badCount + goodCount + neutralCount;
-    return (
-        <div>
-            <h2>Statistics</h2>
-            <p>Good: {goodCount}</p>
-            <p>Neutral: {neutralCount}</p>
-            <p>Bad: {badCount}</p>
-            <p>All: {totalVotes}</p>
-            <p>Average: {(badCount * -1 + goodCount * 1) / totalVotes}</p>
-            <p>Positive: {goodCount / totalVotes}</p>
-        </div>
-    )
+    if (totalVotes === 0) {
+        return (
+            <div>
+                <h2>Statistics</h2>
+                <p>No feedback given yet!</p>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h2>Statistics</h2>
+                <p>Good: {goodCount}</p>
+                <p>Neutral: {neutralCount}</p>
+                <p>Bad: {badCount}</p>
+                <p>All: {totalVotes}</p>
+                <p>Average: {(badCount * -1 + goodCount * 1) / totalVotes}</p>
+                <p>Positive: {goodCount / totalVotes}</p>
+            </div>
+        )
+    }
 };
 
 export default Statistics
